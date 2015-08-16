956dfb8e7dc6fcec30953d766ca06712bc4e7379
Philipp <philipp.sieder@web.de>
2015-08-16 22:12:11 +0200

    Adjust formatting

--- oncard/src/suBay/BonusCreditApplet.java
+++ oncard/src/suBay/BonusCreditApplet.java
@@ -4,70 +4,68 @@ import javacard.framework.*;
 
 public class BonusCreditApplet extends Applet {
 
-    final static byte CLA = (byte) 0xE0; 
-    final static byte INS_ADD_CREDITS = (byte) 0x10; 
-    final static byte INS_SUB_CREDITS = (byte) 0x20; 
-	final static byte INS_CHECK_BALANCE = (byte) 0x30;
-	
-	final static short START_VALUE = 0; 
-	
-	private ShortStore balance;
+    final static byte CLA = (byte) 0xE0;
+    final static byte INS_ADD_CREDITS = (byte) 0x10;
+    final static byte INS_SUB_CREDITS = (byte) 0x20;
+    final static byte INS_CHECK_BALANCE = (byte) 0x30;
 
-	private BonusCreditApplet() {
-		balance = new ShortStore(START_VALUE);
-	}
+    final static short START_VALUE = 0;
 
+    private ShortStore balance;
 
-	//overide
-	public static void install(byte[] bArray, short bOffset, byte bLength) {
-		new BonusCreditApplet().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
-	}
-	
-	//overide
-	public boolean select() { 
-		return true; 
+    private BonusCreditApplet() {
+	balance = new ShortStore(START_VALUE);
     }
-	
-	//overide
-	public void process(APDU apdu) {
-		if (selectingApplet()) {
-			return;
-		}
-		byte[] buffer = apdu.getBuffer(); 
-		if (buffer[ISO7816.OFFSET_CLA] == CLA) { 
-			switch(buffer[ISO7816.OFFSET_INS]) {
-			case INS_ADD_CREDITS:
-						handleAdd(apdu,buffer);
-						break;
-			case INS_SUB_CREDITS:
-						handleRemove(apdu,buffer);
-						break;
-			case INS_CHECK_BALANCE: 
-						handleCheck(apdu,buffer);
-						break;					
-			default:
-				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);	
-			}
-		} else {
-		    ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+
+    // overide
+    public static void install(byte[] bArray, short bOffset, byte bLength) {
+	new BonusCreditApplet().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
+    }
+
+    // overide
+    public boolean select() {
+	return true;
+    }
+
+    // overide
+    public void process(APDU apdu) {
+	if (selectingApplet()) {
+	    return;
+	}
+	byte[] buffer = apdu.getBuffer();
+	if (buffer[ISO7816.OFFSET_CLA] == CLA) {
+	    switch (buffer[ISO7816.OFFSET_INS]) {
+	    case INS_ADD_CREDITS:
+		handleAdd(apdu, buffer);
+		break;
+	    case INS_SUB_CREDITS:
+		handleRemove(apdu, buffer);
+		break;
+	    case INS_CHECK_BALANCE:
+		handleCheck(apdu, buffer);
+		break;
+	    default:
+		ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 	    }
+	} else {
+	    ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
 	}
+    }
 
-	private void handleAdd(APDU apdu, byte[] buffer) {
-		byte lc = buffer[ISO7816.OFFSET_LC];
-		short readBytes = apdu.setIncomingAndReceive(); 
-		this.balance.addValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA));
-	}
+    private void handleAdd(APDU apdu, byte[] buffer) {
+	byte lc = buffer[ISO7816.OFFSET_LC];
+	short readBytes = apdu.setIncomingAndReceive();
+	this.balance.addValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA));
+    }
 
-	private void handleRemove(APDU apdu, byte[] buffer) {
-		byte lc = buffer[ISO7816.OFFSET_LC];
-		short readBytes = apdu.setIncomingAndReceive(); 
-		this.balance.subValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA));
-	}
-	
-		
-	private void handleCheck(APDU apdu, byte[] buffer) {
-		Util.setShort(buffer, (short) 0, this.balance.getValue());
-		apdu.setOutgoingAndSend((short) 0, (short) 0);
-	}
+    private void handleRemove(APDU apdu, byte[] buffer) {
+	byte lc = buffer[ISO7816.OFFSET_LC];
+	short readBytes = apdu.setIncomingAndReceive();
+	this.balance.subValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA));
+    }
+
+    private void handleCheck(APDU apdu, byte[] buffer) {
+	Util.setShort(buffer, (short) 0, this.balance.getValue());
+	apdu.setOutgoingAndSend((short) 0, (short) 2);
+    }
 }
--- oncard/src/suBay/WalletApplet.java
+++ oncard/src/suBay/WalletApplet.java
@@ -4,55 +4,55 @@ import javacard.framework.*;
 
 public class WalletApplet extends Applet {
 
-	final static byte CLA =  (byte) 0xE0;
-    final static byte INS_ADD_MONEY = (byte) 0x10; 
-    final static byte INS_SUB_MONEY = (byte) 0x20; 
-	final static byte INS_CHECK_BALANCE = (byte) 0x30; 
-	
-	final static short START_VALUE = 0;
-	
-	final static short ERROR_INSUFFICIENT_BALANCE = 0x6A83;
-	final static short ERROR_TRANS_EXCEED_MAXIMUM_BALANCE = 0x6A84;
-	
-	private ShortStore moneyc;
-	
-	private WalletApplet() {
-		moneyc = new ShortStore(START_VALUE);
+    final static byte CLA = (byte) 0xE0;
+    final static byte INS_ADD_MONEY = (byte) 0x10;
+    final static byte INS_SUB_MONEY = (byte) 0x20;
+    final static byte INS_CHECK_BALANCE = (byte) 0x30;
+
+    final static short START_VALUE = 0;
+
+    final static short ERROR_INSUFFICIENT_BALANCE = 0x6A83;
+    final static short ERROR_TRANS_EXCEED_MAXIMUM_BALANCE = 0x6A84;
+
+    private ShortStore moneyc;
+
+    private WalletApplet() {
+	moneyc = new ShortStore(START_VALUE);
+    }
+
+    public static void install(byte[] bArray, short bOffset, byte bLength) {
+	new WalletApplet().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
+    }
+
+    public void process(APDU apdu) {
+	if (selectingApplet()) {
+	    return;
 	}
 
-	public static void install(byte[] bArray, short bOffset, byte bLength) {
-		new WalletApplet().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
-	}
-
-	public void process(APDU apdu) {	
-		if (selectingApplet()) {
-			return;
+	byte[] buffer = apdu.getBuffer();
+	if (buffer[ISO7816.OFFSET_CLA] == CLA) {
+	    switch (buffer[ISO7816.OFFSET_INS]) {
+	    case INS_CHECK_BALANCE:
+		Util.setShort(buffer, (short) 0, moneyc.getValue());
+		apdu.setOutgoingAndSend((short) 0, (short) 2);
+		break;
+	    case INS_ADD_MONEY:
+		apdu.setIncomingAndReceive();
+		if (!moneyc.addValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA))) {
+		    CardRuntimeException.throwIt(ERROR_TRANS_EXCEED_MAXIMUM_BALANCE);
 		}
-
-		byte[] buffer = apdu.getBuffer();
-		if (buffer[ISO7816.OFFSET_CLA] == CLA) {
-			switch (buffer[ISO7816.OFFSET_INS]) {			
-			case INS_CHECK_BALANCE:
-				Util.setShort(buffer, (short) 0, moneyc.getValue());
-				apdu.setOutgoingAndSend((short) 0, (short) 2);  
-				break; 		
-			case INS_ADD_MONEY: 
-				apdu.setIncomingAndReceive();
-				if (!moneyc.addValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA))) {
-					CardRuntimeException.throwIt(ERROR_TRANS_EXCEED_MAXIMUM_BALANCE);
-				}
-				break; 
-			case INS_SUB_MONEY: 
-				apdu.setIncomingAndReceive();
-				if (moneyc.subValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA))) {
-					CardRuntimeException.throwIt(ERROR_INSUFFICIENT_BALANCE);
-				}	
-				break; 
-			default:
-				ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
-			}  
-	    } else {
-		    ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
-	    }	
+		break;
+	    case INS_SUB_MONEY:
+		apdu.setIncomingAndReceive();
+		if (moneyc.subValue(Util.getShort(buffer, (short) ISO7816.OFFSET_CDATA))) {
+		    CardRuntimeException.throwIt(ERROR_INSUFFICIENT_BALANCE);
+		}
+		break;
+	    default:
+		ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+	    }
+	} else {
+	    ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
 	}
+    }
 }
