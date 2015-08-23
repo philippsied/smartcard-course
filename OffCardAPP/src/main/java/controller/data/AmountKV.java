3509c8200799138ff69dec5c93afc2c0304a3589
Marcel <Marcel@Marcel-PC.fritz.box>
2015-08-23 14:18:16 +0200

    JavaDoc for AmountKV and TicketEntry

--- OffCardAPP/src/main/java/controller/data/AmountKV.java
+++ OffCardAPP/src/main/java/controller/data/AmountKV.java
@@ -1,27 +1,47 @@
 package controller.data;
 
+/**
+ * 
+ * Klasse zum halten der Einträge für die ComboBox des Geldautomaten
+ *
+ */
 public class AmountKV {
 
+    /**
+     * Anzuzeigender String in der ComboBox
+     */
     private final String mKey;
+
+    /**
+     * Wert für den Eintrag in Cent
+     */
     private final int mAmountInCent;
 
     public AmountKV(int amountInCent) {
 	mKey = String.format("%1$,.2f€", (float) amountInCent / 100);
 	mAmountInCent = amountInCent;
     }
- 
-    
+
+    /**
+     * Rückgabe des Schlüssels für den Eintrag
+     * 
+     * @return Schlüssel als String
+     */
     public String getKey() {
-        return mKey;
-    }
-    
-    public int getAmountInCent() {
-        return mAmountInCent;
+	return mKey;
     }
 
+    /**
+     * Wert in Cent
+     * 
+     * @return Cent als int
+     */
+    public int getAmountInCent() {
+	return mAmountInCent;
+    }
 
     @Override
     public String toString() {
 	return mKey;
-    }   
+    }
 }
